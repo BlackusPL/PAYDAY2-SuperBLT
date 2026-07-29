bfc9df2cf86b6db8ebc8d8f2da4ba93ca9571f6b
HW12Dev <26612968+HW12Dev@users.noreply.github.com>
2026-07-29 15:21:02 +1000

    Added animation auto-upgrader

--- src/assets/convert_animations.cpp
+++ src/assets/convert_animations.cpp
@@ -8,9 +8,22 @@
 
 // Please forgive me, writing a zlib compression routine from scratch is so painful
 #include "fileio/zlibcompression.h"
+#include "util/util.h"
+
+#include "diesel/animation.h"
+
+struct AnimationHeader // 32bit, 64bit has extra padding here on purpose
+{
+	uint32_t type_id;
+	uint32_t version;
+	uint32_t original_location;
+	uint32_t file_size;
+};
 
 std::vector<uint8_t> ConvertAnimation(std::vector<uint8_t>&& data, const std::string& path)
 {
+	if (data.size() < 4)
+		return data;
 
 	if (data[0] == 0x78)
 	{
@@ -29,7 +42,46 @@ std::vector<uint8_t> ConvertAnimation(std::vector<uint8_t>&& data, const std::st
 
 	}
 
-	// TODO: 32bit -> 64bit goes here
+	AnimationHeader* header = (AnimationHeader*)data.data();
 
-    return std::move(data);
+	if ((size_t)header->file_size != data.size()) // size field doesn't align up to have the right data, must be 64bit
+	{
+		return data;
+	}
+	
+	// Parse the contents in 32-bit format
+	diesel::Animation animation;
+	Reader reader((char*)data.data(), data.size(), false);
+
+	if (!animation.ReadUncompressed(reader, diesel::DieselFormatsLoadingParameters(diesel::EngineVersion::PAYDAY_2_LATEST,
+	                                                                   diesel::Renderer::UNSPECIFIED,
+	                                                                   diesel::FileSourcePlatform::WINDOWS_32)))
+	{
+		char msg[512];
+		snprintf(msg, sizeof(msg), "Error occurred while reading 32bit Animation, is the file corrupt? File: %s",
+		         path.c_str());
+		RAIDHOOK_LOG_LOG(msg);
+
+		return data;
+	}
+
+	reader.Close();
+
+	// Now write it back out to our data vector
+
+	Writer writer;
+	MemoryWriterContainer* container = (MemoryWriterContainer*)writer.GetContainer();
+
+	animation.Write(writer,
+	         diesel::DieselFormatsLoadingParameters(diesel::EngineVersion::DIESEL_V3, diesel::Renderer::UNSPECIFIED,
+	                                                diesel::FileSourcePlatform::WINDOWS_64));
+
+	writer.Close();
+
+	// Nasty bodge, I'm sure this is undefined behaviour but it will work here :)
+	std::vector<char> signedData = container->TakeData();
+	std::vector<uint8_t>* aliasingViolationLivesHere = (std::vector<uint8_t>*)&signedData;
+	std::vector<uint8_t> unsignedData = std::move(*aliasingViolationLivesHere);
+
+	return unsignedData;
 }
